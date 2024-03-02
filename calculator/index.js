937b76c0aefda845561a6bcf7ba6047e5dd19bc3
Abdi Kumela <abdikumela036@gmail.com>
2024-03-02 11:38:19 -0800

    handling number and decimal dot

--- calculator/index.js
+++ calculator/index.js
@@ -1,24 +1,60 @@
 const inputValue = document.querySelector(".inputValue");
-const clearBtn = document.querySelector(".clear");
-const deleteBtn = document.querySelector(".delete");
+const clearBtn = document.querySelector(".clear"); //done
+const deleteBtn = document.querySelector(".delete"); //done
 const equalBtn = document.querySelector(".btnEqual");
 
-const squareBtn = document.querySelector(".square");
-const addBtn = document.querySelector(".addition");
-const subBtn = document.querySelector(".substraction");
-const divBtn = document.querySelector(".division");
-const multBtn = document.querySelector(".multiplication");
+const squareBtn = document.querySelector(".square"); //done
 const decimalBtn = document.querySelector(".decimalPoint");
+const operandBtn = document.querySelectorAll(".operand");
+const numbersBtn = document.querySelectorAll(".numbers");
 
 // Handling Events
 
-// clear the input
+// clear All the inputvalue
+inputValue.value = 0;
+
 clearBtn.addEventListener("click", () => {
-  inputValue.value = "";
+  inputValue.value = 0;
 });
 
 // delete value one by one
 deleteBtn.addEventListener("click", () => {
   const newValue = inputValue.value.slice(0, -1);
   inputValue.value = newValue;
+  if (!inputValue.value) inputValue.value = 0;
+});
+
+// handling Square
+squareBtn.addEventListener("click", () => {
+  if (!inputValue.value) return;
+  else {
+    inputValue.value = inputValue.value * inputValue.value;
+  }
+});
+
+// handling numbers and signs
+numbersBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const clickedBtn = btn.value;
+    const currentValue = inputValue.value;
+
+    const updatedValue =
+      currentValue === "0" ? clickedBtn : currentValue + clickedBtn;
+    inputValue.value = updatedValue;
+  });
+});
+
+// handling dot
+
+decimalBtn.addEventListener("click", () => {
+  if (inputValue.value.includes(decimalBtn.value)) return;
+  else {
+    inputValue.value += decimalBtn.value;
+  }
+});
+
+// handling operands
+
+operandBtn.forEach((opp) => {
+  opp.addEventListener("click", () => {});
 });
