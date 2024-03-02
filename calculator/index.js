f71b705d58327bd6eabbc30623f0c43e0064838e
Abdi Kumela <abdikumela036@gmail.com>
2024-03-02 09:23:20 -0800

    handle delete and clear events

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,24 @@
-"use strict";
+const inputValue = document.querySelector(".inputValue");
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+const equalBtn = document.querySelector(".btnEqual");
+
+const squareBtn = document.querySelector(".square");
+const addBtn = document.querySelector(".addition");
+const subBtn = document.querySelector(".substraction");
+const divBtn = document.querySelector(".division");
+const multBtn = document.querySelector(".multiplication");
+const decimalBtn = document.querySelector(".decimalPoint");
+
+// Handling Events
+
+// clear the input
+clearBtn.addEventListener("click", () => {
+  inputValue.value = "";
+});
+
+// delete value one by one
+deleteBtn.addEventListener("click", () => {
+  const newValue = inputValue.value.slice(0, -1);
+  inputValue.value = newValue;
+});
