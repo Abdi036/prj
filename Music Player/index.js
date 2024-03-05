b10286009c8d2257566dd760e9b3afd61742521a
Abdi Kumela <abdikumela036@gmail.com>
2024-03-05 01:24:51 -0800

    add play functionality

--- Music Player/index.js
+++ Music Player/index.js
@@ -1,1 +1,25 @@
 "use strict";
+const music = document.querySelector("audio");
+const previousBtn = document.querySelector(".fa-backward");
+const playBtn = document.querySelector(".fa-play");
+const nextBtn = document.querySelector(".fa-forward");
+
+// function to handle music play
+
+let isPlaying = false;
+function playMusic() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  music.play();
+}
+
+function pauseMusic() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+
+  music.pause();
+}
+
+playBtn.addEventListener("click", () =>
+  isPlaying ? pauseMusic() : playMusic()
+);
